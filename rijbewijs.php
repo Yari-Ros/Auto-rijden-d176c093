87fa6e9fb51cc59ae3099cf4a9f4d5b416af1a36
Yari-Ros <2118088@talnet.nl>
2019-11-15 13:57:06 +0100

    Uitwerking van de exercise

---
+++ rijbewijs.php
@@ -0,0 +1,10 @@
+<?php
+
+$input =readline ("Ok, dus jij wilt je rijbewijs halen huh? nou leuk nieuws voor jou dat kan, je moet me wel even vertellen hoe oud je bent. En geef me gewoon je leeftijd anders mag je oprotten.");
+
+
+if ($input<16.5)
+    echo ("btfo scrub");
+
+else
+    echo ("Dear sirs, now, we hast done it. We hast made a quarter of a million subscribers, 250,000, yet grows still. Tis the fact such count wast done anon ist phenomenal. Amazed, I-I am. For aiding this channel and helping it grow, I request my dearest thanks for all of thee. I-I love thee... Awesome, arst thee.");
